2366bee400206216d77578b3ef721116e5ed34b6
Marek Petak <marekpetak@kannegiesser.com>
2017-03-31 16:03:00 +0100

    Routing highlights

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
+  styles: [`
+    .active {
+      color: orange !important;
+    }
+  `],
   templateUrl: './app/app.component.html'
 })
 
