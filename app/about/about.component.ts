9515afccd470cce795413b79309742746df8d8aa
Marek Petak <marekpetak@kannegiesser.com>
2017-04-03 15:59:36 +0100

    TEsting

--- app/about/about.component.ts
+++ app/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'about-page',
     template: `
-        about page
+        about page :)
     `
 })
-export class AboutComponent {}
+export class AboutComponent { }
